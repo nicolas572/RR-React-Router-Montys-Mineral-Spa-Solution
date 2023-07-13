7124e1c8665c1c6c103d26363d79cbd395267005
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-07-12 21:24:10 -0700

    finished activity

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
-  
+
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
 
 
@@ -19,24 +21,32 @@ function App() {
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
           <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
+            <Container>
+              <Nav defaultActiveKey="/" variant="tabs" fill>
+                <Nav.Item>
+                  <Nav.Link href="/">
+                    <Link to="/">Home</Link>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item >
+                  <Nav.Link eventKey={"aboutPage"}>
+                    <Link to="/about">About Us</Link>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item >
+                  <Nav.Link eventKey={"packagesPage"}>
+                    <Link to="/packages">Our Packages</Link>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
           </div>
         </header>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route path="/packages" render={() => <Packages packages={packages} />} />
         </div>
 
       </Router>
